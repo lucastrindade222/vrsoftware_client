8b5b26556cbf3cdcfda9657f79cf6a2728cc7316
lucas <lucas500000000@gmail.com>
2022-10-06 11:32:15 -0300

    create project

---
+++ src/main/java/vrsoftware_client/main.java
@@ -0,0 +1,24 @@
+package teoria;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Teoria {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        }
+
+
+
+
+    }
+
+
